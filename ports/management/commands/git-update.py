f5e34b24ce68c12995a303489ab1979af117d267
arjunsalyan <arjunsalyan@icloud.com>
2019-07-07 00:06:26 +0530

    Make the git-update command ready to run updates using rsync

--- ports/management/commands/git-update.py
+++ ports/management/commands/git-update.py
@@ -1,6 +1,13 @@
+import subprocess
+import json
+
 from django.core.management.base import BaseCommand, CommandError
 
 from parsing_scripts import git_update
+from ports.models import Port
+
+RSYNC_SOURCE = "rsync://rsync.macports.org/macports//trunk/dports/PortIndex_darwin_16_i386/PortIndex.json"
+JSON_FILE = "portindex.json"
 
 
 class Command(BaseCommand):
@@ -17,5 +24,18 @@ class Command(BaseCommand):
                             help="Not recommended. Helps you provide a commit from which update should start")
 
     def handle(self, *args, **options):
-        git_update.get_list_of_changed_ports(options['new'], options['old'])
+        if options['new'] is False:
+            # Fetch the latest version of portindex2json.json
+            subprocess.call(["rsync", RSYNC_SOURCE, JSON_FILE])
+
+            # Open the JSON file to find the latest commit
+            with open(JSON_FILE, "r") as file:
+                data = json.load(file)
+
+            # set new hash to the hash obtained from json file
+            options['new'] = data['info']['commit']
+
+        ports_to_be_updated = git_update.get_list_of_changed_ports(options['new'], options['old'])
+        Port.update(ports_to_be_updated)
+
 
