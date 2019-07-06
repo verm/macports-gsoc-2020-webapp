2ddb088d80324dd81e6f9e39c1387558f4413f53
arjunsalyan <arjunsalyan@icloud.com>
2019-07-07 00:40:37 +0530

    Make the load command perform populating using rsync

--- ports/management/commands/load.py
+++ ports/management/commands/load.py
@@ -1,6 +1,12 @@
+import subprocess
+import json
+
 from django.core.management.base import BaseCommand, CommandError
 
-from ports.models import Port
+from ports.models import Port, Commit
+
+RSYNC_SOURCE = "rsync://rsync.macports.org/macports//trunk/dports/PortIndex_darwin_16_i386/PortIndex.json"
+JSON_FILE = "portindex.json"
 
 
 class Command(BaseCommand):
@@ -12,7 +18,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            Port.load(options['path'])
+            if not Port.objects.count() > 0:
+                if options['path'] == "rsync":
+                    # Fetch the latest version of portindex2json.json
+                    subprocess.call(["rsync", RSYNC_SOURCE, JSON_FILE])
+
+                    # Start loading
+                    Port.load(JSON_FILE)
+
+                    # Open the JSON file to find the latest commit
+                    with open(JSON_FILE, "r") as file:
+                        data = json.load(file)
+                    commit = data['info']['commit']
+
+                    # Write this commit to database
+                    Commit.objects.create(hash=commit)
+
+                else:
+                    Port.load(options['path'])
         except FileNotFoundError:
             raise CommandError('"{}" not found. Make sure "{}" is a valid JSON file and is in the root of the project'.format(
                 options['path'], options['path']
